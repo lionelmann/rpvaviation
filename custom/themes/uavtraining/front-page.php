cfceea040469ce84ebda720b99bb46b13d9c435a
Lionel Mann <lionelmann@gmail.com>
2019-03-07 20:30:11 -0500

    get post content

--- custom/themes/uavtraining/front-page.php
+++ custom/themes/uavtraining/front-page.php
@@ -4,6 +4,8 @@
         <div class="container" >
         <div class="row section ">
             <div class="col s12 m10 offset-m1 about">
+
+
                 <p>RPV Aviation is operated by Transport Canada Certified flight instructors and pilots. We bring years of manned aviation best practice to UAV operations to ensure the isdustry will thrive and follow the same success.</p>
                 <p>UAV's are real flying vehicles that share our airspace, they present enormous potential logistical and safety challenges. Transport Canada regulates UAV operations in Canada and requires all operators hold a Special Flight Operation Certificate (SFOC). In fact, failure to comply with these regulations could result in substantial penalties, seizure of equipment and shut-down of operations. </p><p>More importantly, operators who are not properly trained and certified pose a significant danger to other aircraft and could cause catastrophic incidents. Drone Flight School provides our clients with the essential background knowledge and expertise that ensure safe legal operations.</p>
                 <p>Given their low cost compared to regular manned aerial solutions, UAV operations are increasing around the world.</p>
@@ -18,6 +20,13 @@
                 <li>Military</li>
                 <li>Photography, video and movie production</li>
                 </ul>
+
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                the_content();
+                endwhile; else: ?>
+                <p>Sorry, no posts matched your criteria.</p>
+                <?php endif; ?>
+
             </div>
         </div>
     </div>
